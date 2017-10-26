0469e1a61f91f67978fe535cfedad298db5704ff
Miller, Payne <pmiller@cardinalsolutions.com>
2017-10-26 14:52:34 -0400

    Creates skeleton for Login Activity

---
+++ FDAndroidClient/src/main/java/com/freedomotic/freedomotic/activity/LoginActivity.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2009-2017 Freedomotic team http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package com.freedomotic.freedomotic.activity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.freedomotic.freedomotic.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.email) AutoCompleteTextView usernameTextView;
+
+    @BindView(R.id.password) EditText passwordEditText;
+
+    @BindView(R.id.login_progress) View progressView;
+
+    @BindView(R.id.login_form) View loginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @OnClick(R.id.email_sign_in_button)
+    void attemptLogin() {
+        resetErrors();
+
+        String email = usernameTextView.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        boolean areCredentialsValid = false;
+        View invalidField = null;
+
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordEditText.setError(getString(R.string.error_invalid_password));
+            invalidField = passwordEditText;
+            areCredentialsValid = true;
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            usernameTextView.setError(getString(R.string.error_field_required));
+            invalidField = usernameTextView;
+            areCredentialsValid = true;
+        } else if (!isEmailValid(email)) {
+            usernameTextView.setError(getString(R.string.error_invalid_email));
+            invalidField = usernameTextView;
+            areCredentialsValid = true;
+        }
+
+        if (areCredentialsValid) {
+            invalidField.requestFocus();
+        } else {
+            showProgress(true);
+        }
+    }
+
+    private void resetErrors() {
+        usernameTextView.setError(null);
+        passwordEditText.setError(null);
+    }
+
+    private boolean isEmailValid(String email) {
+        return true;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            loginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            progressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+}
+
